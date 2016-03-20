fe61d20ce4a2300f0f32024b8b7f13d67c64999e
ChrisBoon <cboon27@gmail.com>
2016-03-20 18:13:55 -0400

    Inline critical css
    Signed-off-by: ChrisBoon <cboon27@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,6 +134,21 @@ module.exports = function(grunt) {
         clean: {
             images: ['build/**/*.{png,jpg,gif}'],
             css: ['build/**/*.{css,css.map}']
+        },
+
+
+        critical: {
+            dist: {
+                options: {
+                    inline: true,
+                    base: './build',
+                    minify: true,
+                    width: 1300,
+                    height: 900
+                },
+                src: 'build/index.html',
+                dest: 'build/index.html'
+            }
         }
 
     });
@@ -148,6 +163,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-critical');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', [
